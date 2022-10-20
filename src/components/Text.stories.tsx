c5ff2105147344c455f335cc04b7624f723f9300
pattyheiden <patriciasilvalimah@gmail.com>
2022-10-20 16:35:50 -0300

    estilizacao de texto

---
+++ src/components/Text.stories.tsx
@@ -0,0 +1,33 @@
+import { Text, TextProps } from './Text'
+import { Meta, StoryObj } from '@storybook/react'
+
+export default{
+    title: 'Components/Text',
+    component: Text,
+    args: {
+        children: 'Lorem Ipsum.'
+    },
+} as Meta<TextProps>
+
+export const Default: StoryObj<TextProps> = {}
+
+export const Small : StoryObj<TextProps> = {
+    args: {
+        size: 'sm'
+    }
+}
+
+export const Large : StoryObj<TextProps> = {
+    args: {
+        size: 'lg'
+    }
+}
+
+export const CustomComponent : StoryObj<TextProps> = {
+    args: {
+        asChild: true,
+        children: (
+            <p>Testando</p>
+        )
+    }
+}
